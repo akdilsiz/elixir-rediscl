056d0595a0e3546935b809c83c628a0643302cb8
ghroo2 <akdilsiz@tecpor.com>
2018-07-21 19:24:05 +0300

    Added stop superv func

--- lib/mix/rediscl.ex
+++ lib/mix/rediscl.ex
@@ -1,11 +1,21 @@
 defmodule Mix.Rediscl do
 	@moduledoc false
-
+	alias Rediscl.Superv
 	@doc false
   @spec ensure_started(module, Keyword.t) :: {:ok, pid} | no_return
 	def ensure_started(client, _opts) do
-		{:ok, started} = Application.ensure_all_started(client)
+		{:ok, _started} = Application.ensure_all_started(client)
 
-		{:ok, started}
+		case Superv.start_link([name: {:local, Rediscl},
+                      worker_module: __MODULE__.Work,
+                      size: 1,
+                      max_overflow: @pool_max_overflow]) do
+    	{:ok, pid} ->
+    		{:ok, pid}
+  		{:error, {:already_started, pid}} ->
+				{:ok, pid}
+			{:error, error} ->
+				{:error, error}             	
+    end
 	end
 end
