ec3ac8014af4768040c47782fa1198b7af7ace04
ghroo2 <akdilsiz@tecpor.com>
2018-07-21 19:29:41 +0300

    Added pool_size opts Mix.Rediscl ensure_started

--- lib/mix/rediscl.ex
+++ lib/mix/rediscl.ex
@@ -3,13 +3,14 @@ defmodule Mix.Rediscl do
 	alias Rediscl.Superv
 	@doc false
   @spec ensure_started(module, Keyword.t) :: {:ok, pid} | no_return
-	def ensure_started(client, _opts) do
+	def ensure_started(client, opts) do
 		{:ok, _started} = Application.ensure_all_started(client)
 
+		pool_size = Keyword.get(opts, :pool_size, 1)
 		case Superv.start_link([name: {:local, Rediscl},
                       worker_module: __MODULE__.Work,
-                      size: 1,
-                      max_overflow: @pool_max_overflow]) do
+                      size: pool_size,
+                      max_overflow: 1]) do
     	{:ok, pid} ->
     		{:ok, pid}
   		{:error, {:already_started, pid}} ->
