b6cf456764053ba8a4a0d50a6ca89a44925f4945
ghroo2 <akdilsiz@tecpor.com>
2018-07-22 13:40:19 +0300

    Test conf fix

--- config/test.exs
+++ config/test.exs
@@ -3,9 +3,9 @@ use Mix.Config
 config :logger, level: :warn
 
 config :rediscl,
-	host: "127.0.0.1",
+	host: "localhost",
 	port: 6379,
-	password: "1093d67d7ad14ef76ee1a7b7dbd2027bb2b865a2237366948762ae9bedae802c",
-	database: 10,
+	password: "",
+	database: 0,
 	pool: 5,
 	timeout: 15_000
